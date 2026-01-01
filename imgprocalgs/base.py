68dae715795301338c40bfb37aee16bd5d82be71
Harsh-from-teenShikari <rajputharsh843@gmail.com>
2026-01-01 23:11:19 +0530

    Create BaseAlgorithm class for image processing

---
+++ imgprocalgs/base.py
@@ -0,0 +1,14 @@
+import abc
+from imgprocalgs.algorithms.utilities import Image
+
+class BaseAlgorithm(metaclass=abc.ABCMeta):
+    """ Base class for all algorithms """
+    def __init__(self, image_path: str, destination_path: str):
+        self.image_path = image_path
+        self.destination_path = destination_path
+        self.image = Image(image_path)
+    
+    @abc.abstractmethod
+    def process(self):
+        """ Run the algorithm processing """
+        pass
